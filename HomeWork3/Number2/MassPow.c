b68447371d1396373a6be812e764e61863bf7b2e
ChastovSlava <crychs142@gmail.com>
2022-10-05 20:52:37 +0300

    Удалены прототипы программы program.exe. Также исправлены некоторые баги. Внесены правки в щадачу номер 2.

--- HomeWork3/Number2/MassPow.c
+++ HomeWork3/Number2/MassPow.c
@@ -6,6 +6,7 @@ int main() {
 	int e, result = 0, mass[100000];
 	printf("¬ведите кол-во эл-ов (до 100000): \n");
 	scanf_s("%d", &e);
+	system("cls");
 	printf("¬ведите эл-ты массива: \n");
 	for (int i = 0; i < e; i++) {
 		scanf_s("%d", &mass[i]);
@@ -13,7 +14,7 @@ int main() {
 	for (int i = 0; i < e; i++) {
 		result = result + pow(mass[i], 2);
 	}
-	printf("—умма квадратов эл-та равна: %d.\n", result);
+	printf("—умма квадратов эл-тов массива равна: %d.\n", result);
 	system("pause");
 	return 0;
 }
